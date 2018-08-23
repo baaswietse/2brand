ec62fb57a8b2331fc564d8c12a15c6556f9aed8d
Wietse Delclef <wietse.delclef@gmail.com>
2018-08-23 10:30:24 +0000

    mail bug test for deployment

--- newpostmail.js
+++ newpostmail.js
@@ -4,7 +4,9 @@ const nodemailer = require('nodemailer');
 
 function sendmail(post){
     var transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: 'smtp.gmail.com',
+      port: '465',
+      secure: true,
       auth: {
         user: 'wietse.spam69@gmail.com',
         pass: 'W942018d'
@@ -15,9 +17,9 @@ function sendmail(post){
     })
     
     
-    const mailOptions = {
+    var mailOptions = {
       from: 'wietse.spam69@gmail.com', // sender address
-      to: 'info@2brand.be', // list of receivers
+      to: 'wietse.delclef@gmail.com', // list of receivers
       subject: 'Nieuwe post door @' + post.instaname, // Subject line
       html: '<table> <tbody style="font-size: 15x"> <tr> <th align="left">Instagram: </th> <td>' + post.instaname + '</td> </tr> <tr> <th align="left">email: </th> <td>' + post.email + '</td> </tr> <tr> <th align="left">partner: </th> <td>' + post.partner + '</td> </tr> <tr> <th align="left">voordeel: </th> <td>' + post.voordeel + '</td> </tr> <tr> <th align="left">link: </th> <td>'+ post.link +'</td> </tr> </tbody></table>'
     };
